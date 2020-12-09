f88ced2bf2671d6e8f941d32741397f6c1fd45de
balqis97 <55779824+balqis97@users.noreply.github.com>
2020-12-09 12:41:39 +0800

    Update web.php

--- web.php
+++ web.php
@@ -4,7 +4,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ebookController;
 
-use App\Http\Controllers\catController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,43 +20,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/about', 'App\Http\Controllers\catController@about');
-
-//Route::get('/team', 'App\Http\Controllers\catController@team');
-
-//Route::get('/services', 'App\Http\Controllers\catController@services');
-
 
 Route::get('/about', 'App\Http\Controllers\ebookController@about');
 Route::get('/list', 'App\Http\Controllers\ebookController@list');
 
 
-/*
-//route pass variable as associative array
-Route::get('/{name}', function ($name) {
-
-    return view('welcome',['name'=>$name]);
-});
-
-/*
-Route::get('/hello', function () {
-    return view('hello');
-});
-
-/*
-Route::get('/cat/morecats ', function () {
-    return 'This is a cat ';
-});
-*/
-
-/*
-Route::get('/cat', function () {
-    return view('layout/about.blade');
-});
-*/
-
-/*
-Route::get('/:{catname}/:{id}', function ($catname,$id) {
-    return 'This is '. $catname. ' and the id is ' . $id;
-});
-*/
